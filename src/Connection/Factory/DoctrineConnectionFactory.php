2a6fb9b87fada5662bcb2d93aa2380b3792473ce
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-06-12 17:17:13 +0300

    Added Doctrine connection classes

---
+++ src/Connection/Factory/DoctrineConnectionFactory.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Vainyl
+ *
+ * PHP Version 7
+ *
+ * @package   Doctrine-orm-bridge
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://vainyl.com
+ */
+declare(strict_types=1);
+
+namespace Vainyl\Doctrine\ORM\Connection\Factory;
+
+use Vainyl\Connection\ConnectionInterface;
+use Vainyl\Core\AbstractIdentifiable;
+use Vainyl\Doctrine\ORM\Connection\DoctrinePostgresqlConnection;
+use Vainyl\Doctrine\ORM\Exception\UnknownDoctrineDriverTypeException;
+
+/**
+ * Class DoctrineConnectionFactory
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class DoctrineConnectionFactory extends AbstractIdentifiable
+{
+    private $connection;
+
+    /**
+     * DoctrineConnectionFactory constructor.
+     *
+     * @param ConnectionInterface $connection
+     */
+    public function __construct(ConnectionInterface $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getName() : string
+    {
+        return 'doctrine';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function createConnection(string $connectionName) : ConnectionInterface
+    {
+        $type = 'pgsql';
+        switch ($type) {
+            case 'pgsql':
+                return new DoctrinePostgresqlConnection($this->connection);
+                break;
+            case 'mysql':
+                return new DoctrinePostgresqlConnection($this->connection);
+                break;
+            default:
+                throw new UnknownDoctrineDriverTypeException($this, $type);
+        }
+    }
+}
