8ba80ee2b46f2199ecbcdbda0cac12f907d61851
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-06-12 17:36:20 +0300

    Added Doctrine extension registartion

---
+++ src/Extension/DoctrineOrmExtension.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Vainyl
+ *
+ * PHP Version 7
+ *
+ * @package   Doctrine-orm-bridge
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://vainyl.com
+ */
+declare(strict_types=1);
+
+namespace Vainyl\Doctrine\ORM\Extension;
+
+use Vainyl\Core\Extension\AbstractExtension;
+
+/**
+ * Class DoctrineOrmExtension
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class DoctrineOrmExtension extends AbstractExtension
+{
+}
