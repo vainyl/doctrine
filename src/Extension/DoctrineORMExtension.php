8faab542054048d135b35e3276b2c30ce0c7576b
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-06-13 15:56:45 +0300

    Added database.entity parsing

--- src/Extension/DoctrineORMExtension.php
+++ src/Extension/DoctrineORMExtension.php
@@ -12,6 +12,10 @@ declare(strict_types=1);
 
 namespace Vainyl\Doctrine\ORM\Extension;
 
+use Symfony\Component\DependencyInjection\Alias;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Vainyl\Core\Application\EnvironmentInterface;
+use Vainyl\Core\Exception\MissingRequiredServiceException;
 use Vainyl\Core\Extension\AbstractExtension;
 
 /**
@@ -21,4 +25,23 @@ use Vainyl\Core\Extension\AbstractExtension;
  */
 class DoctrineORMExtension extends AbstractExtension
 {
+    /**
+     * @inheritDoc
+     */
+    public function load(
+        array $configs,
+        ContainerBuilder $container,
+        EnvironmentInterface $environment = null
+    ): AbstractExtension {
+        if (false === $container->hasDefinition('database.entity')) {
+            throw new MissingRequiredServiceException($container, 'database.entity');
+        }
+
+        $definition = $container->getDefinition('database.entity');
+        if ($definition->isSynthetic()) {
+            $container->set('database.document', new Alias('database.document.doctrine'));
+        }
+
+        return parent::load($configs, $container, $environment);
+    }
 }
