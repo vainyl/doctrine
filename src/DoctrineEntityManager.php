1516e11c2ed05f0e257d175b1d5c96022d51eeb0
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-08-11 12:20:27 +0300

    Fix default limit 0 problem

--- src/DoctrineEntityManager.php
+++ src/DoctrineEntityManager.php
@@ -131,7 +131,7 @@ class DoctrineEntityManager extends EntityManager implements DomainStorageInterf
         int $limit = 0,
         int $offset = 0
     ): array {
-        return $this->getRepository($name)->findBy($criteria, $orderBy, $limit, $offset);
+        return $this->getRepository($name)->findBy($criteria, $orderBy, $limit ? $limit : null, $offset);
     }
 
     /**
