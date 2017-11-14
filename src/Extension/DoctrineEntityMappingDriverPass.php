9878a9336357a320dac71c2be088e7314e804c15
Andrey Dembitskiy <andrey.dembitskiy@cosmonova.net>
2017-11-14 13:04:40 +0200

    :bug: Fix typos in services, phpDoc

--- src/Extension/DoctrineEntityMappingDriverPass.php
+++ src/Extension/DoctrineEntityMappingDriverPass.php
@@ -4,7 +4,7 @@
  *
  * PHP Version 7
  *
- * @package   Doctrine-odm-bridge
+ * @package   Doctrine-orm-bridge
  * @license   https://opensource.org/licenses/MIT MIT License
  * @link      https://vainyl.com
  */
